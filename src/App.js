49191e6bbe55b11b70b153c7fdd8984f64266693
Fagner Lopes <fagnernlopes@gmail.com>
2021-04-05 18:32:51 -0300

    Country and Header components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
+import Countries from './components/countries/Countries';
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      allCountries: [],
+    }
+  }
+
+  async componentDidMount(){
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    const json = await res.json();
+
+    const allCountries = json.map(({ name, numericCode, flag, population}) => {
+      return {
+        id: numericCode,
+        name,
+        flag,
+        population
+      }
+    });
+
+    this.setState({
+      allCountries: allCountries,
+    });
+  }
   render() {
-    return <ProjetoBase />;
+    const { allCountries } = this.state;
+    return (
+      <div className="container">
+        <h1>React Countries</h1>
+        <Countries countries={allCountries}/>
+      </div>
+    );
   }
 }
