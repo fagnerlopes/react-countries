c0050b2454463358bbbe6ce5ffa49a0bb6e33d7b
Fagner Lopes <fagnernlopes@gmail.com>
2021-04-06 09:22:32 -0300

    Includes countries flags and css style in the Country component

--- src/components/countries/Countries.js
+++ src/components/countries/Countries.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
-import Country from './Country';
+import React, { Component } from "react";
+import Country from "./Country";
+import css from "./countries.module.css";
 
 export default class Countries extends Component {
   render() {
     const { countries } = this.props;
     return (
-      <div>
-        <ul>
-          {
-            countries.map(country =>{
-              return <li key={country.id}><Country country={country}/></li>
-            })
-          }
-        </ul>
+      <div  className={`${css.flexRow}`}>
+          {countries.map((country) => {
+            return (
+              <Country key={country.id} country={country} />
+            );
+          })}
       </div>
-    )
+    );
   }
 }
--- src/components/countries/Country.js
+++ src/components/countries/Country.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import css from './countries.module.css';
 
 export default class Country extends Component {
   render() {
-    const { country } = this.props;
+    const { country, key } = this.props;
+    console.log(country);
 
     return (
-      <div>
+      <div className={css.country}>
+        <img src={country.flag} alt={country.name}/>
         {country.name}
       </div>
     )
